e8a5dbb81215170f1282152f65711893c9c7d669
Chimoha31 <mokochii1108@gmail.com>
2022-05-02 22:56:13 -0700

    UserCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import UserList from './features/users/UserList';
+import React from "react";
+import UserList from "./features/users/UserList";
+import { Routes, Route } from "react-router-dom";
+import AddUser from "./features/users/AddUser";
 
 function App() {
   return (
-    <div>
-       <UserList />
+    <div className="container mx-auto px-2 max-x-5xl pt-10 md:pt-32">
+      <h1 className="text-center font-bold text-2xl text-gray-700">
+        User Card
+      </h1>
+      <Routes>
+        <Route exact path="/" element={<UserList />} />
+        <Route path="/add-user" element={<AddUser />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/features/users/AddUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddUser = () => {
+  return (
+    <div className="mt-10 max-w-xl mx-auto">
+      <p>Add User</p>
+    </div>
+  )
+}
+
+export default AddUser
